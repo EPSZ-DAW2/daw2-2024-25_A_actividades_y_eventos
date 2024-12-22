26e0259e866ecb3909968a375588bacf34eb1543
MarcosCabDel <marcos.cabrero@usal.es>
2024-12-22 19:42:47 +0100

    Base de datos creada y enlazada

--- codigo/config/db_proyecto_A.php
+++ codigo/config/db_proyecto_A.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=daw2_2024_A_xxxxx',
+    'dsn' => 'mysql:host=localhost;dbname=actividades_eventos',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
