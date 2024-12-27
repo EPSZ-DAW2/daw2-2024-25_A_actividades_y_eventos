7df68ff870ef38c6a88b4b9925588c489ff06e65
Angelo <111171031+Angelo-Eyama@users.noreply.github.com>
2024-12-27 10:49:31 +0100

    Generado CRUD de usuarios
    Incluye su correspondiente modelo, vista y controlador

---
+++ codigo/views/usuarios/_form.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\Usuarios $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="usuarios-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'nick')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'contraseña')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'apellidos')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'fecha_nacimiento')->textInput() ?>
+
+    <?= $form->field($model, 'direccion')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'ubicacion')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'activo')->textInput() ?>
+
+    <?= $form->field($model, 'fecha_hora_registro')->textInput() ?>
+
+    <?= $form->field($model, 'registro_confirmado')->textInput() ?>
+
+    <?= $form->field($model, 'revisado')->textInput() ?>
+
+    <?= $form->field($model, 'ultimo_acceso')->textInput() ?>
+
+    <?= $form->field($model, 'intentos_acceso')->textInput() ?>
+
+    <?= $form->field($model, 'bloqueado')->textInput() ?>
+
+    <?= $form->field($model, 'fecha_hora_bloqueo')->textInput() ?>
+
+    <?= $form->field($model, 'motivo_bloqueo')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'valoracion_usuario')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'notas')->textarea(['rows' => 6]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ codigo/views/usuarios/view.php
@@ -0,0 +1,55 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/** @var yii\web\View $this */
+/** @var app\models\Usuarios $model */
+
+$this->title = $model->id;
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Usuarios'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
+?>
+<div class="usuarios-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'nick',
+            'contraseña',
+            'email:email',
+            'nombre',
+            'apellidos',
+            'fecha_nacimiento',
+            'direccion',
+            'ubicacion',
+            'activo',
+            'fecha_hora_registro',
+            'registro_confirmado',
+            'revisado',
+            'ultimo_acceso',
+            'intentos_acceso',
+            'bloqueado',
+            'fecha_hora_bloqueo',
+            'motivo_bloqueo:ntext',
+            'valoracion_usuario',
+            'notas:ntext',
+        ],
+    ]) ?>
+
+</div>
