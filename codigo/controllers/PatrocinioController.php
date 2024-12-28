451c60c1fe7a81bf5f811418ab0aed35f45fe855
17davidjs <17davidj.s@usal.es>
2024-12-28 13:49:28 +0100

    modificado crud patrocinadores y añadido documento de datos para la base de datos

--- codigo/controllers/PatrocinioController.php
+++ codigo/controllers/PatrocinioController.php
@@ -1,35 +1,52 @@
 <?php
 namespace app\controllers;
+
 use Yii;
 use yii\web\Controller;
 use app\models\Patrocinadores;
 use app\models\PatrocinadoresForm;
 
-
-class PatrocinioController extends Controller {
-    
-    
-        
+class PatrocinioController extends Controller
+{
     public function actionIndex()
     {
-        $patrocinadores = Patrocinadores::find()->all(); // Uso de ActiveRecord para obtener los patrocinadores
+        $patrocinadores = (new Patrocinadores())->getPatrocinadores(); // Uso del método getPatrocinadores para obtener los patrocinadores
         return $this->render('index', [
             'patrocinadores' => $patrocinadores
         ]);
-    } // actionIndex
-    
+    }
+
     public function actionAdd()
     {
         $model = new PatrocinadoresForm();
-        $title = "Añadir nuevos patrocinadores";
+        $title = "Añadir nuevo patrocinador";
 
         if ($model->load(Yii::$app->request->post()) && $model->validate()) {
             $patrocinador = new Patrocinadores();
-            $patrocinador->nombre = $model->nombre;
-            $patrocinador->apellido = $model->apellido;
-            $patrocinador->email = $model->email;
+            $patrocinador->nick = $model->nick;
             $patrocinador->password = $model->password;
+            $patrocinador->email = $model->email;
+            $patrocinador->nombre = $model->nombre;
+            $patrocinador->apellidos = $model->apellidos;
+            $patrocinador->fecha_nacimiento = $model->fecha_nacimiento;
+            $patrocinador->ubicacion = $model->ubicacion;
+            $patrocinador->activo = $model->activo;
+            $patrocinador->fecha_registro = $model->fecha_registro;
+            $patrocinador->registro_confirmado = $model->registro_confirmado;
+            $patrocinador->revisado = $model->revisado;
+            $patrocinador->ultimo_acceso = $model->ultimo_acceso;
+            $patrocinador->intentos_acceso = $model->intentos_acceso;
+            $patrocinador->bloqueado = $model->bloqueado;
+            $patrocinador->fecha_bloqueo = $model->fecha_bloqueo;
+            $patrocinador->motivo_bloqueo = $model->motivo_bloqueo;
+            $patrocinador->notas = $model->notas;
             $patrocinador->save(); // Guarda el nuevo patrocinador
+
+            // Añadir el rol de patrocinador en la tabla roles
+            Yii::$app->db->createCommand()->insert('roles', [
+                'nombre_usuario' => $patrocinador->nick,
+                'rol' => 'patrocinador',
+            ])->execute();
 
             return $this->redirect(['index']); // Redirige a la página principal
         }
@@ -38,44 +55,19 @@ class PatrocinioController extends Controller {
             'title' => $title,
             'model' => $model
         ]);
-    } // actionAdd
-    
-    public function actionModificar($id)
-    {
-        $patrocinador = Patrocinadores::findOne($id);
+    }
 
-        if (!$patrocinador) {
-            throw new \yii\web\NotFoundHttpException('Patrocinador no encontrado');
-        }
 
-        $model = new PatrocinadoresForm();
-        $model->setAttributes($patrocinador->attributes); // Establece los datos actuales del patrocinador
-
-        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            $patrocinador->nombre = $model->nombre;
-            $patrocinador->apellido = $model->apellido;
-            $patrocinador->email = $model->email;
-            $patrocinador->password = $model->password;
-            $patrocinador->save(); // Guarda los cambios
-
-            return $this->redirect(['index']); // Redirige a la página principal
-        }
-
-        return $this->render('formulario', [
-            'title' => "Editar Patrocinador " . $id,
-            'model' => $model,
-            'patrocinador' => $patrocinador
-        ]);
-    } // actionModificar
-    
     public function actionEliminar($id)
     {
         $patrocinador = Patrocinadores::findOne($id);
         if ($patrocinador) {
-            $patrocinador->delete(); // Elimina el patrocinador
+            // Eliminar el rol de patrocinador en la tabla roles
+            Yii::$app->db->createCommand()->delete('roles', ['nombre_usuario' => $patrocinador->nick])->execute();
+            // Eliminar el patrocinador de la tabla usuario
+            $patrocinador->delete();
         }
 
-        return $this->redirect(['index']); // Redirige a la página principal
-    } // actionEliminar
+        return $this->redirect(['index']);
+    }
 }
-
--- codigo/views/Patrocinio/index.php
+++ codigo/views/Patrocinio/index.php
@@ -3,11 +3,15 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 
+/* @var $this yii\web\View */
+/* @var $patrocinadores app\models\Patrocinadores[] */
+
+$this->title = 'Lista de Patrocinadores';
 ?>
 
 <div class="row">
     <div class="col-9">
-        <h2 style="display: inline;">Opciones para patrocinadores</h2>
+        <h2 style="display: inline;">Opciones para patrocinadors</h2>
     </div>
     <div class="col-3 text-end">
         <a class="btn btn-success" href="<?= Url::to(['patrocinio/add']) ?>">
@@ -21,22 +25,48 @@ use yii\helpers\Url;
     <thead>
         <tr>
             <th>ID</th>
-            <th>NOMBRE</th>
-            <th>APELLIDOS</th>
-            <th>EMAIL</th>
-            <th>PASSWORD</th>
-            <th>EDITAR</th>
-            <th>ELIMINAR</th>
+            <th>Nick</th>
+            <th>Password</th>
+            <th>Email</th>
+            <th>Nombre</th>
+            <th>Apellidos</th>
+            <th>Fecha Nacimiento</th>
+            <th>Ubicación</th>
+            <th>Activo</th>
+            <th>Fecha Registro</th>
+            <th>Registro Confirmado</th>
+            <th>Revisado</th>
+            <th>Último Acceso</th>
+            <th>Intentos Acceso</th>
+            <th>Bloqueado</th>
+            <th>Fecha Bloqueo</th>
+            <th>Motivo Bloqueo</th>
+            <th>Notas</th>
+            <th>Editar</th>
+            <th>Eliminar</th>
         </tr>
     </thead>
     <tbody>
         <?php foreach ($patrocinadores as $patrocinador): ?>
             <tr>
                 <td><?= Html::encode($patrocinador->id) ?></td>
-                <td><?= Html::encode($patrocinador->nombre) ?></td>
-                <td><?= Html::encode($patrocinador->apellido) ?></td>
-                <td><?= Html::encode($patrocinador->email) ?></td>
+                <td><?= Html::encode($patrocinador->nick) ?></td>
                 <td><?= Html::encode(substr($patrocinador->password, 0, 18)) ?></td>
+                <td><?= Html::encode($patrocinador->email) ?></td>
+                <td><?= Html::encode($patrocinador->nombre) ?></td>
+                <td><?= Html::encode($patrocinador->apellidos) ?></td>
+                <td><?= Html::encode($patrocinador->fecha_nacimiento) ?></td>
+                <td><?= Html::encode($patrocinador->ubicacion) ?></td>
+                <td><?= Html::encode($patrocinador->activo ? 'Sí' : 'No') ?></td>
+                <td><?= Html::encode($patrocinador->fecha_registro) ?></td>
+                <td><?= Html::encode($patrocinador->registro_confirmado ? 'Sí' : 'No') ?></td>
+                <td><?= Html::encode($patrocinador->revisado ? 'Sí' : 'No') ?></td>
+                <td><?= Html::encode($patrocinador->ultimo_acceso) ?></td>
+                <td><?= Html::encode($patrocinador->intentos_acceso) ?></td>
+                <td><?= Html::encode($patrocinador->bloqueado ? 'Sí' : 'No') ?></td>
+                <td><?= Html::encode($patrocinador->fecha_bloqueo) ?></td>
+                <td><?= Html::encode($patrocinador->motivo_bloqueo) ?></td>
+                <td><?= Html::encode($patrocinador->notas) ?></td>
                 <td>
                     <a class="btn btn-primary" href="<?= Url::to(['patrocinio/modificar', 'id' => $patrocinador->id]) ?>">
                         Editar
